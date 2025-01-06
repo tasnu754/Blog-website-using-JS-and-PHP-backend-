5e0a339a3c83d3832c620dafa0c7c1c28472bdcd
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2025-01-06 19:52:38 +0600

    add user functionality implemented, add users in database

--- admin/index.php
+++ admin/index.php
@@ -21,6 +21,8 @@ include 'partials/header.php';
             <h5>Manage Post</h5>
             </a>
           </li>
+
+          <?php if(isset($_SESSION['user_is_admin'])) : ?>
           <li>
             <a href="add-user.php"><i class="uil uil-user-plus"></i>
             <h5>Add User</h5>
@@ -40,6 +42,7 @@ include 'partials/header.php';
             <h5>Manage Categories</h5>
             </a>
           </li>
+          <?php endif ?>
         </ul>
       </aside>
 
