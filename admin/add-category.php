216fe0b2b9bbde08caef242df4ada9a2218cd020
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2025-01-06 22:33:47 +0600

    add categories to the database and handle input errors

--- admin/add-category.php
+++ admin/add-category.php
@@ -1,21 +1,33 @@
 <?php
 include 'partials/header.php';
+
+// get back form data if invalid
+$title = $_SESSION['add-category-data']['title'] ?? null;
+$description = $_SESSION['add-category-data']['description'] ?? null;
+
+unset($_SESSION['add-category-data']);
+
 ?>
 
 
-    <section class="form_section">
-        <div class="container form_section-container">
-          <h2>Add Category</h2>
-          <div class="alert_message error">
-            <p>This is an error message.</p>
-          </div>
-          <form action="">
-            <input type="text" placeholder="Title">
-            <textarea rows="4" placeholder="Description"></textarea>
-            <button type="submit" class="btn">Add Category</button>
-          </form>
-        </div>
-      </section>
+<section class="form_section">
+  <div class="container form_section-container">
+    <h2>Add Category</h2>
+    <?php if (isset($_SESSION['add-category'])) : ?>
+      <div class="alert_message error">
+        <p>
+          <?= $_SESSION['add-category']; ?>
+          <?php unset($_SESSION['add-category']); ?>
+        </p>
+      </div>
+    <?php endif ?>
+    <form action="<?= ROOT_URL ?>admin/add-category-logic.php" method="POST">
+      <input type="text" name="title" value="<?= $title ?>" placeholder="Title">
+      <textarea rows="4" value="<?= $description ?>" name="description" placeholder="Description"></textarea>
+      <button type="submit" name="submit" class="btn">Add Category</button>
+    </form>
+  </div>
+</section>
 
 <?php
 include '../partials/footer.php';
