8d981bab411e24a90aa39824d89a3ed22de5277a
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2025-01-07 21:13:56 +0600

    created post table in database and implemented add post/create post functionalities

--- admin/add-post.php
+++ admin/add-post.php
@@ -1,39 +1,50 @@
 <?php
 include 'partials/header.php';
+
+// fetch categories from database
+$query = "SELECT * FROM categories";
+
+$categories = mysqli_query($connection, $query);
+
 ?>
 
 
 
-    <section class="form_section">
-        <div class="container form_section-container">
-          <h2>Add Post</h2>
-          <div class="alert_message error">
-            <p>This is an error message.</p>
-          </div>
-          <form action="" enctype="multipart/form-data">
-            <input type="text" placeholder="Title">
-            <select>
-              <option value="1">Travel</option>
-              <option value="1">Art</option>
-              <option value="1">Science & Technology</option>
-              <option value="1">Travel</option>
-              <option value="1">Travel</option>
-            </select>
-            <textarea rows="6" placeholder="Body"></textarea>
+<section class="form_section">
+  <div class="container form_section-container">
+    <h2>Add Post</h2>
+    <?php if (isset($_SESSION['add-post'])) : ?>
+    <div class="alert_message error container">
+      <p>
+        <?= $_SESSION['add-post']; ?>
+        <?php unset($_SESSION['add-post']); ?>
+      </p>
+    </div>
+  <?php endif ?>
+    <form action="<?= ROOT_URL ?>admin/add-post-logic.php" enctype="multipart/form-data" method="POST">
+      <input type="text" name="title" placeholder="Title">
+      <select name="category">
+        <?php while ($category = mysqli_fetch_assoc($categories)) : ?>
+          <option value="<?= $category['id'] ?>"><?= $category['title'] ?></option>
+        <?php endwhile; ?>
+      </select>
+      <textarea rows="6" name="body" placeholder="Body"></textarea>
 
-            <div class="form_control inline">
-                <input type="checkbox" id="is_featured" checked>
-                <label for="is_featured" >Featured</label>
-            </div>
-              
-              <div class="form_control">
-                <label for="thumbnail">Add Thumbnail</label>
-                <input type="file" id="thumbnail">
-              </div>
-            <button type="submit" class="btn">Add Post</button>
-          </form>
+      <?php if (isset($_SESSION['user_is_admin'])): ?>
+        <div class="form_control inline">
+          <input type="checkbox" name="is_featured" value="1" id="is_featured" checked>
+          <label for="is_featured">Featured</label>
         </div>
-      </section>
+      <?php endif; ?>
+
+      <div class="form_control">
+        <label for="thumbnail">Add Thumbnail</label>
+        <input type="file" name="thumbnail" id="thumbnail">
+      </div>
+      <button type="submit" name="submit" class="btn">Add Post</button>
+    </form>
+  </div>
+</section>
 
 <?php
 include '../partials/footer.php';
