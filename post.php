d888546ac25f05020b3b7f09ad3aeabd7f5420f8
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2025-01-08 19:51:11 +0600

    fetching single post, fetching post by category

--- post.php
+++ post.php
@@ -2,6 +2,18 @@
 
 include 'partials/header.php';
 
+// fetch post from database if id is set
+if (isset($_GET['id'])) {
+    $id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
+    $query = "SELECT * FROM posts WHERE id=$id";
+    $result = mysqli_query($connection, $query);
+    $post = mysqli_fetch_assoc($result);
+} else {
+    header('location: ' . ROOT_URL . 'blog.php');
+    die();
+}
+
+
 ?>
 
 
@@ -10,26 +22,34 @@ include 'partials/header.php';
 
     <section class="singlepost">
         <div class="container singlepost_container">
-          <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, dicta.</h2>
+          <h2><?= $post['title']?></h2>
+
           <div class="post_author">
+            <?php
+            // fetch author from users table using author_id
+
+            $author_id = $post['author_id'];
+            $author_query = "SELECT * FROM users WHERE id=$author_id";
+
+            $author_result = mysqli_query($connection, $author_query);
+            $author = mysqli_fetch_assoc($author_result);
+            ?>
             <div class="post_author-avatar">
-              <img src="./images/alex.jpg">
+              <img src="./images/<?= $author['avatar'] ?>" alt="Author Avatar">
             </div>
+
             <div class="post_author-info">
-              <h5>By: Jane Doe</h5>
-              <small>June 10, 2022 07:23</small>
+              <h5>By: <?= "{$author['firstname']} {$author['lastname']}" ?></h5>
+              <small><?= date("M d, Y H:i", strtotime($post['date_time'])) ?></small>
             </div>
           </div>
           <div class="singlepost_thumbnail">
-            <img src="./images/alex.jpg" alt="">
+            <img src="./images/<?=$post['thumbnail']?>" alt="">
           </div>
 
           
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente cupiditate, accusamus at minus consequuntur magnam quidem. Ex neque rerum ratione sint id expedita dicta facilis explicabo hic dolor? Libero, cumque.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente cupiditate, accusamus at minus consequuntur magnam quidem. Ex neque rerum ratione sint id expedita dicta facilis explicabo hic dolor? Libero, cumque.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente cupiditate, accusamus at minus consequuntur magnam quidem. Ex neque rerum ratione sint id expedita dicta facilis explicabo hic dolor? Libero, cumque.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente cupiditate, accusamus at minus consequuntur magnam quidem. Ex neque rerum ratione sint id expedita dicta facilis explicabo hic dolor? Libero, cumque.</p>
-        </div>
+        <p><?= $post['body']?></p>
+         </div>
 
     </section>
 
