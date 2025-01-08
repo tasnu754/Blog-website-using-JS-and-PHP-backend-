14b8946965e0282dd438dcbadd7394754aad0035
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2025-01-08 20:01:06 +0600

    fetching post by category

--- category-posts.php
+++ category-posts.php
@@ -2,188 +2,75 @@
 
 include 'partials/header.php';
 
+// fetch posts if id is set
+if (isset($_GET['id'])) {
+    $id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
+    $query = "SELECT * FROM posts WHERE category_id=$id ORDER BY date_time DESC";
+    $result = mysqli_query($connection, $query);
+} else {
+    header('location: ' . ROOT_URL . 'blog.php');
+    die();
+}
 ?>
 
 
     <header class="category_title">
-        <h2>Category Title</h2>
+    <h2>
+    <?php
+          // fetch category from categories table using category_id of post
+
+          $category_id = $id;
+          $category_query = "SELECT * FROM categories WHERE id=$id";
+          $category_result = mysqli_query($connection, $category_query);
+          $category = mysqli_fetch_assoc($category_result);
+          echo $category['title'];
+          ?>
+        </h2>
       </header>
 
 
 
-    <section class="posts">
-        <div class="container posts_container">
-          <article class="post">
-            <div class="post_thumbnail">
-              <img src="./images/alex.jpg" alt="Blog Post Image">
-            </div>
-      
-            <div class="post_info">
-              <a href="category-posts.html" class="category_button">Wild Life</a>
-      
-              <h3><a href="post.html" class="post_title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, odit.</a></h3>
-      
-              <p class="post_body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad suscipit dolore dicta? Accusantium officiis hic modi corrupti expedita laborum laboriosam.</p>
-      
-              <div class="post_author">
-                <div class="post_author-avatar">
-                  <img src="./images/alex.jpg" alt="Author Avatar">
-                </div>
-      
-                <div class="post_author-info">
-                  <h5>By: John Mills</h5>
-                  <small>June 13, 2022 - 18:34</small>
-                </div>
-              </div>
-            </div>
-          </article>
-          <article class="post">
-            <div class="post_thumbnail">
-              <img src="./images/alex.jpg" alt="Blog Post Image">
-            </div>
-      
-            <div class="post_info">
-              <a href="#" class="category_button">Wild Life</a>
-      
-              <h3><a href="post.html" class="post_title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, odit.</a></h3>
-      
-              <p class="post_body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad suscipit dolore dicta? Accusantium officiis hic modi corrupti expedita laborum laboriosam.</p>
-      
-              <div class="post_author">
-                <div class="post_author-avatar">
-                  <img src="./images/alex.jpg" alt="Author Avatar">
-                </div>
-      
-                <div class="post_author-info">
-                  <h5>By: John Mills</h5>
-                  <small>June 13, 2022 - 18:34</small>
-                </div>
-              </div>
-            </div>
-          </article>
-          <article class="post">
-            <div class="post_thumbnail">
-              <img src="./images/alex.jpg" alt="Blog Post Image">
-            </div>
-      
-            <div class="post_info">
-              <a href="#" class="category_button">Wild Life</a>
-      
-              <h3><a href="post.html" class="post_title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, odit.</a></h3>
-      
-              <p class="post_body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad suscipit dolore dicta? Accusantium officiis hic modi corrupti expedita laborum laboriosam.</p>
-      
-              <div class="post_author">
-                <div class="post_author-avatar">
-                  <img src="./images/alex.jpg" alt="Author Avatar">
-                </div>
-      
-                <div class="post_author-info">
-                  <h5>By: John Mills</h5>
-                  <small>June 13, 2022 - 18:34</small>
-                </div>
-              </div>
-            </div>
-          </article>
-          <article class="post">
-            <div class="post_thumbnail">
-              <img src="./images/alex.jpg" alt="Blog Post Image">
-            </div>
-      
-            <div class="post_info">
-              <a href="#" class="category_button">Wild Life</a>
-      
-              <h3><a href="post.html" class="post_title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, odit.</a></h3>
-      
-              <p class="post_body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad suscipit dolore dicta? Accusantium officiis hic modi corrupti expedita laborum laboriosam.</p>
-      
-              <div class="post_author">
-                <div class="post_author-avatar">
-                  <img src="./images/alex.jpg" alt="Author Avatar">
-                </div>
-      
-                <div class="post_author-info">
-                  <h5>By: John Mills</h5>
-                  <small>June 13, 2022 - 18:34</small>
-                </div>
-              </div>
-            </div>
-          </article>
-          <article class="post">
-            <div class="post_thumbnail">
-              <img src="./images/alex.jpg" alt="Blog Post Image">
-            </div>
-      
-            <div class="post_info">
-              <a href="#" class="category_button">Wild Life</a>
-      
-              <h3><a href="post.html" class="post_title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, odit.</a></h3>
-      
-              <p class="post_body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad suscipit dolore dicta? Accusantium officiis hic modi corrupti expedita laborum laboriosam.</p>
-      
-              <div class="post_author">
-                <div class="post_author-avatar">
-                  <img src="./images/alex.jpg" alt="Author Avatar">
-                </div>
-      
-                <div class="post_author-info">
-                  <h5>By: John Mills</h5>
-                  <small>June 13, 2022 - 18:34</small>
-                </div>
-              </div>
-            </div>
-          </article>
-          <article class="post">
-            <div class="post_thumbnail">
-              <img src="./images/alex.jpg" alt="Blog Post Image">
-            </div>
-      
-            <div class="post_info">
-              <a href="#" class="category_button">Wild Life</a>
-      
-              <h3><a href="post.html" class="post_title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, odit.</a></h3>
-      
-              <p class="post_body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad suscipit dolore dicta? Accusantium officiis hic modi corrupti expedita laborum laboriosam.</p>
-      
-              <div class="post_author">
-                <div class="post_author-avatar">
-                  <img src="./images/alex.jpg" alt="Author Avatar">
-                </div>
-      
-                <div class="post_author-info">
-                  <h5>By: John Mills</h5>
-                  <small>June 13, 2022 - 18:34</small>
-                </div>
-              </div>
-            </div>
-          </article>
-          <article class="post">
-            <div class="post_thumbnail">
-              <img src="./images/alex.jpg" alt="Blog Post Image">
-            </div>
-      
-            <div class="post_info">
-              <a href="#" class="category_button">Wild Life</a>
-      
-              <h3><a href="post.html" class="post_title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, odit.</a></h3>
-      
-              <p class="post_body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad suscipit dolore dicta? Accusantium officiis hic modi corrupti expedita laborum laboriosam.</p>
-      
-              <div class="post_author">
-                <div class="post_author-avatar">
-                  <img src="./images/alex.jpg" alt="Author Avatar">
-                </div>
-      
-                <div class="post_author-info">
-                  <h5>By: John Mills</h5>
-                  <small>June 13, 2022 - 18:34</small>
-                </div>
-              </div>
-            </div>
-          </article>
+      <section class="posts">
+  <div class="container posts_container">
+    <?php while ($post = mysqli_fetch_assoc($result)) : ?>
+      <article class="post">
+        <div class="post_thumbnail">
+          <img src="./images/<?= $post['thumbnail'] ?>" alt="Blog Post Image">
         </div>
-      </section>
 
+        <div class="post_info">
+
+
+          <h3><a href="<?= ROOT_URL ?>post.php?id=<?= $post['id'] ?>" class="post_title"><?= $post['title'] ?></a></h3>
+
+          <p class="post_body"> <a href="<?= ROOT_URL ?>post.php?id=<?= $post['id'] ?>" class="post_title"><?= substr($post['body'], 0, 150) ?>...</a></p>
+
+          <div class="post_author">
+            <?php
+            // fetch author from users table using author_id
+
+            $author_id = $post['author_id'];
+            $author_query = "SELECT * FROM users WHERE id=$author_id";
+
+            $author_result = mysqli_query($connection, $author_query);
+            $author = mysqli_fetch_assoc($author_result);
+            ?>
+            <div class="post_author-avatar">
+              <img src="./images/<?= $author['avatar'] ?>" alt="Author Avatar">
+            </div>
+
+            <div class="post_author-info">
+              <h5>By: <?= "{$author['firstname']} {$author['lastname']}" ?></h5>
+              <small><?= date("M d, Y H:i", strtotime($post['date_time'])) ?></small>
+            </div>
+          </div>
+        </div>
+      </article>
+    <?php endwhile ?>
+
+
+  </div>
+</section>
 
 
 
